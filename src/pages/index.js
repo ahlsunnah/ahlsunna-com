a3021015e73d0e85fb473f59fef4d165f12e0cfb
abumalick <abdullah.hilson@gmail.com>
2020-03-25 12:25:40 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>بسم الله الرحمن الرحيم</h1>
+    <p>This website is under construction</p>
+  </Layout>
+)
+
+export default IndexPage
